5140be35be43e2b883887f6d02d0dc2792259368
CharlesKasasira <charleskasasira01@gmail.com>
2021-10-27 18:10:50 +0300

    lifting state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import React from 'react'
+import './css/styles.css'
 
 function App() {
+
+  const [theme, switchTheme] = React.useState('')
+
+  const handleClick = switchTo => {
+    switchTheme(switchTo)
+    console.log(theme)
+  }
+
   return (
-    <div className="App">
-    
+    <div className={`container ${theme}`}>
+      <h1>The theme is <SayTheme themeName={theme} /></h1>
+      <div>
+        <input type="button" className="btn black" onClick={() => handleClick('black')} />
+        <input type="button" className="btn blue" onClick={() => handleClick('blue')} />
+        <input type="button" className="btn yellow" onClick={() => handleClick('yellow')} />
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+function SayTheme (props){
+  return <span>{props.themeName}</span>
+}
